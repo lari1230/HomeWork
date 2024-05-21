13e92564eb26d1ea814ee5483dee3d9072c90765
Ivan <Ivan@DESKTOP-CTOEP4J>
2024-05-21 16:03:51 +0300

    I did HomeWork 15.05.2024

---
+++ 15.05.2024/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+public interface First
+{
+    void First();
+}
+public interface Second
+{
+    void Second();
+}
+public delegate void Del();
+public class X : First, Second
+{
+    private Del Da { get; set; }
+    private event Del delHandler
+    {
+        add { Da += value; }
+        remove { Da -= value; }
+    }
+    public X()
+    {
+        delHandler += A;
+        delHandler += B;
+    }
+    void First.First()
+    {
+        A();
+    }
+    private void A()
+    {
+        Console.WriteLine("A");
+    }
+    void Second.Second()
+    {
+        B();
+    }
+    private void B()
+    {
+        Console.WriteLine("B");
+    }
+    public void InvokeDelegates()
+    {
+        Da.Invoke();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        X x = new X();
+
+        x.InvokeDelegates();
+    }
+}
