d235e9351f41e08f76ebdb7c214b33bf68d1aae1
Студент <@Y3-6>
2024-06-20 18:52:23 +0300

    2.06.24 was been done

---
+++ 2.06.2024/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace _2._06._2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            progressBar1 = new ProgressBar();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 2);
+            tableLayoutPanel1.Controls.Add(button1, 0, 1);
+            tableLayoutPanel1.Controls.Add(listBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(progressBar1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(3, 421);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(394, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(3, 212);
+            button1.Name = "button1";
+            button1.Size = new Size(394, 203);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += SearchButton_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(403, 3);
+            listBox1.Name = "listBox1";
+            tableLayoutPanel1.SetRowSpan(listBox1, 3);
+            listBox1.Size = new Size(394, 444);
+            listBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Dock = DockStyle.Bottom;
+            progressBar1.Location = new Point(3, 183);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(394, 23);
+            progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ 2.06.2024/Form1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _2._06._2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo directory = new DirectoryInfo(dialog.SelectedPath);
+                string searchPattern = textBox1.Text;
+                string searchDirectory = directory.FullName;
+
+                string[] files = Directory.GetFiles(searchDirectory, searchPattern, SearchOption.AllDirectories);
+
+                progressBar1.Maximum = files.Length;
+                progressBar1.Value = 0;
+
+                foreach (string file in files)
+                {
+                    listBox1.Items.Add(Path.GetFileName(file));
+                    progressBar1.Value++;
+                }
+            }
+            
+        }
+    }
+}
